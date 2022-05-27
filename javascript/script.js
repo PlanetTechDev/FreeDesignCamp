6a84e910422f7e9d9f2aaddc38e61cce235b41b4
PlanetTechDev <104599631+PlanetTechDev@users.noreply.github.com>
2022-05-26 22:39:04 -0400

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -14,3 +14,14 @@ if (displaySetting == 'block') {
     
     
 }
+
+var number = document.getElementById('number');
+
+number.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
