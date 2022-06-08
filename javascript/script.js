d30cf70cac43afe060c1f7a11a9bf834f0916404
PlanetTechDev <104599631+PlanetTechDev@users.noreply.github.com>
2022-06-08 15:38:30 -0400

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,54 @@ number.onkeydown = function(e) {
     }
 }
 
+// Database Test
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
+<script type="text/javascript">
+  var onloadCallback = function() {
+    alert("grecaptcha is ready!");
+  };
+</script>
+
+document.getElementById(open-menu)
+
+// Open Menu Mobile 
+
+function openmenu() {
+  var x = document.getElementById("mobile-menu_r");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display="block";
+  }
+}
+
+var d = new Date().getDay();
+var titles = ['title 1', 'title 2', 'title 3', 'title 4', 'title 5', 'title 6', 'title 7'];
+
+document.getElementsByClassName("mobile-menu_msg").innerHTML = titles[d-1];
+
+function openmenu() {
+  var x = document.getElementById("mobile-menu_r");
+  if (x.style.display === "block") {
+  x.style.display = "none";
+  } else {
+  x.style.display="block";
+  }
+}
